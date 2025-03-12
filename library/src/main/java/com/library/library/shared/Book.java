b92b6cb3d55a3c04f71d5f0e49500d6d14329b5d
Akshay Lodha <akshayslodha@gmail.com>
2025-03-12 20:18:37 +0100

    Add an Entity

---
+++ library/src/main/java/com/library/library/shared/Book.java
@@ -0,0 +1,16 @@
+package main.java.com.library.library.shared;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String status; // e.g., AVAILABLE, BORROWED
+}
